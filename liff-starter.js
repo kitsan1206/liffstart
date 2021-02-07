aa5be761ad3324d52d380f90dd20c529d75bd7e2
kitsan1206 <78648510+kitsan1206@users.noreply.github.com>
2021-02-08 08:36:43 +0900

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const useNodeJS = false;   // if you are not using a node server, set this value to false
-    const defaultLiffId = "https://liff.line.me/1655648104-WDAmjddv";   // change the default LIFF value if you are not using a node server
+    const defaultLiffId = "1655648104-WDAmjddv";   // change the default LIFF value if you are not using a node server
 
     // DO NOT CHANGE THIS
     let myLiffId = "";
